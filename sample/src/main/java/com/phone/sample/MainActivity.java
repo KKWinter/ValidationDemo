62e5c83138e6d66424ffef1e02daee1b40daf5eb
黄冬 <you@example.com>
2018-12-26 16:56:12 +0800

    update .gradle

--- sample/src/main/java/com/phone/sample/MainActivity.java
+++ sample/src/main/java/com/phone/sample/MainActivity.java
@@ -20,6 +20,7 @@ import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 /**
@@ -38,6 +39,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+
+
         setContentView(R.layout.activity_main);
         context = getApplicationContext();
         check();
